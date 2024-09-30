d80350c6a3fb3532a6edca5e588ea613f9d7d92e
daemonflynn777 <nick_denisov_777@mail.ru>
2024-10-01 01:44:18 +0300

    Test xlstm model

--- xlstm_moex/models/__init__.py
+++ xlstm_moex/models/__init__.py
@@ -2,3 +2,4 @@
 # https://github.com/styalai/xLSTM-pytorch
 # https://github.com/muslehal/xLSTMTime/tree/main
 # https://github.com/myscience/x-lstm
+# https://coderzcolumn.com/tutorials/artificial-intelligence/pytorch-lstm-networks-for-time-series-regression-tasks
